2b64727246af26e627a84b5048eec2850eabd02c
tsukit <tsukit@191e848b-5d75-4687-f8cb-5a6eca54552a>
2012-02-10 09:11:09 +0000

    more stuff to parser. found scanner fail.

--- src/hasthon/Parser.hs
+++ src/hasthon/Parser.hs
@@ -48,6 +48,7 @@ parse mode input =
 -- single input grammar
 singleInputGrammar :: Parser ABSTree
 singleInputGrammar = do 
+   fail ""
    return ABSTree
 
 -- file input grammar
@@ -59,22 +60,85 @@ fileInputGrammar = do
 -- eval input grammar
 evalInputGrammar :: Parser ABSTree
 evalInputGrammar = do 
+   fail ""
    return ABSTree
 
 -- statement
 stmt :: Parser ABSTree
-stmt = simpleStmt<|> compoundStmt
+stmt = simpleStmt <|> compoundStmt
 
 -- simple statement
 simpleStmt :: Parser ABSTree
 simpleStmt = do
+   firstStmt <- smallStmt
+   otherStmts <- many (pDEL ";" >> smallStmt >>= return)
+   optional $ pDEL ";"
+   pNEWLINE
    return ABSTree
 
 -- compound statement statement
 compoundStmt :: Parser ABSTree
 compoundStmt = do
+   fail ""
    return ABSTree
 
+-- small statement
+smallStmt :: Parser ABSTree
+smallStmt = do
+   pKW "for"
+   return ABSTree
+
+
+
+
+
+
+
+
+-- utility function to help parsing token
+tok :: Token -> Parser Token
+tok t@(Token typ _) = tokenPrim showTok nextPos testTok
+   where showTok t'                 = show t'
+         nextPos p t' ts            = incSourceColumn p 1
+         testTok t'@(Token typ' _)  = if tokcmp typ typ' then Just t' else Nothing
+
+toks :: [Token] -> Parser [Token]
+toks = tokens showTok nextPos 
+   where showTok ts'   = show ts'
+         nextPos p ts' = incSourceColumn p $ length ts'
+
+-- utility to help comparing token type but not its actual value for some type
+tokcmp (TTIndent      _)               (TTIndent _)                       = True
+tokcmp (TTIndent      _)               _                                  = False
+tokcmp (TTDedent      _)               (TTDedent _)                       = True
+tokcmp (TTDedent      _)               _                                  = False
+tokcmp (TTIdentifier  _)               (TTIdentifier _)                   = True
+tokcmp (TTIdentifier  _)               _                                  = False
+tokcmp (TTLiteral (LTString       _))  (TTLiteral (LTString      _))      = True
+tokcmp (TTLiteral (LTBytes        _))  (TTLiteral (LTBytes       _))      = True
+tokcmp (TTLiteral (LTInteger      _))  (TTLiteral (LTInteger     _))      = True
+tokcmp (TTLiteral (LTFloat        _))  (TTLiteral (LTFloat       _))      = True
+tokcmp (TTLiteral (LTImaginary  _ _))  (TTLiteral (LTImaginary _ _))      = True
+tokcmp (TTLiteral _                 )  (TTLiteral _                )      = False
+tokcmp t1                           t2                                    = t1 == t2
+
+-- utility to make a token only for the part we care about
+mktok :: TokenType -> Token
+mktok tt = (Token tt (0,0))
+
+-- various token parsers
+pINDENT        = tok $ mktok $ TTIndent 0
+pDEDENT        = tok $ mktok $ TTDedent 0
+pNEWLINE       = tok $ mktok TTNewline
+pKW s          = tok $ mktok $ TTKeyword s
+pID            = tok $ mktok $ TTIdentifier ""
+pLTSTR         = tok $ mktok $ TTLiteral $ LTString ""
+pLTBYTES       = tok $ mktok $ TTLiteral $ LTBytes ""
+pLTINTEGER     = tok $ mktok $ TTLiteral $ LTInteger ""
+pLTFLOAT       = tok $ mktok $ TTLiteral $ LTFloat ""
+pLTIMAGINARY   = tok $ mktok $ TTLiteral $ LTImaginary "" ""
+pOP s          = tok $ mktok $ TTOperator s
+pDEL s         = tok $ mktok $ TTDelimeter s
 
 
 
