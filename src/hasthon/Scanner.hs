bfbe250694c1604db02d991bcd67693fed609664
tsukit <tsukit@191e848b-5d75-4687-f8cb-5a6eca54552a>
2012-05-26 09:08:35 +0000

    finish hex encoding. Still need to work on oct encoding

--- src/hasthon/Scanner.hs
+++ src/hasthon/Scanner.hs
@@ -16,6 +16,7 @@ import Text.Parsec.Prim
 import Text.Parsec.Char
 import Text.Parsec.String
 import Text.Parsec.Pos
+import Text.Parsec.Error
 import qualified Data.Set as Set
 import Data.List
 import Text.Parsec.Error
@@ -24,6 +25,8 @@ import Control.Monad
 import Text.PrettyPrint hiding (char)
 import Hasthon.Common
 import Data.Maybe
+import Data.Char
+import Data.Bits
 
 -- token type
 data TokenType = TTIndent Int
@@ -227,21 +230,29 @@ stringToken = do
          shortStrChar     = noneOf ['\\', '\n']
          longStrChar      = noneOf ['\\']
          strEscapeSeq     = char '\\' >> anyChar >>= return . (\c -> '\\' : c : [])
-         convert p s      = if isJust p then return s else performConvert s
-         performConvert s = case s of
-                               ('\\' : '\n' : r)         -> performConvert r
-                               ('\\' : '\'' : r)         -> performConvert r >>= return . ('\'':)
-                               ('\\' : '"'  : r)         -> performConvert r >>= return . ('"':)
-                               ('\\' : 'a'  : r)         -> performConvert r >>= return . ('\a':)
-                               ('\\' : 'b'  : r)         -> performConvert r >>= return . ('\b':)
-                               ('\\' : 'f'  : r)         -> performConvert r >>= return . ('\f':)
-                               ('\\' : 'n'  : r)         -> performConvert r >>= return . ('\n':)
-                               ('\\' : 't'  : r)         -> performConvert r >>= return . ('\t':)
-                               ('\\' : 'v'  : r)         -> performConvert r >>= return . ('\v':)
-                               ('\\' : 'x'  : r)         -> performConvert r >>= return . ('\v':)
-                               ('\\' : r)                -> performConvert r >>= return . ('\v':)
-                               (l    : r)                -> performConvert r >>= return . (l:)
-                               []                        -> return []
+         convert :: Maybe Char -> String -> Parser String
+         convert p s      = if isJust p then return s else doConvert s 0
+         doConvert :: String -> Int -> Parser String
+         doConvert s p    = case s of
+                               ('\\' : l : r)   -> 
+                                    case (l:r) of
+                                       ('\n' : r)  -> doConvert r (p+2)
+                                       ('\'' : r)  -> doConvert r (p+2) >>= return . ('\'':)
+                                       ('"'  : r)  -> doConvert r (p+2) >>= return . ('"':)
+                                       ('a'  : r)  -> doConvert r (p+2) >>= return . ('\a':)
+                                       ('b'  : r)  -> doConvert r (p+2) >>= return . ('\b':)
+                                       ('f'  : r)  -> doConvert r (p+2) >>= return . ('\f':)
+                                       ('n'  : r)  -> doConvert r (p+2) >>= return . ('\n':)
+                                       ('t'  : r)  -> doConvert r (p+2) >>= return . ('\t':)
+                                       ('v'  : r)  -> doConvert r (p+2) >>= return . ('\v':)
+                                       ('x'  : r)  -> doConvertHex r (p+2)
+                                       r           -> doConvertOct r p
+                               (l    : r)       -> doConvert r (p+1) >>= return . (l:)
+                               []               -> return []
+         doConvertHex (h1:h2:r) p | isHexDigit h1 && isHexDigit h2 = doConvert r (p+2) >>= return . ((chr $ toHex (h1:h2:[])):)
+                                  | otherwise                      = fail $ "escaped ecode error at position: " ++ show p ++ "-" ++ show (p+2)
+         doConvertOct s p = doConvert s (p+1)
+         toHex s          = let l = (length s) - 1 in foldl (\a (c,p) -> (digitToInt c `shiftL` (4*p)) + a) 0 (zip s [l,l-1,0])
                                
                                
    
